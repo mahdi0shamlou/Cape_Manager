ff6c8008bbf5d372b3e94b6912760d561df9bd36
Mahdi Shamlou <shamlouprogrammer@gmail.com>
2025-01-19 12:47:01 +0000

    getting machines list and their status and then pass it and create and update test for status

--- Web_App/status.py
+++ Web_App/status.py
@@ -1,7 +1,24 @@
 from fastapi import APIRouter
+import libvirt
 
 router = APIRouter()
 
 @router.get("/status/")
 async def read_status():
-    return {"name": "Status Name"}
+    conn = libvirt.open('qemu:///system')
+    if conn is None:
+        raise Exception("Failed to open connection to qemu:///system")
+    domains = conn.listAllDomains()
+
+    vm_status_list = []
+
+    for domain in domains:
+        vm_info = {
+            'name': domain.name(),
+            'id': domain.ID(),
+            'state': domain.state()[0]  # state returns a tuple (state, reason)
+        }
+        vm_status_list.append(vm_info)
+
+    conn.close()
+    return vm_status_list
