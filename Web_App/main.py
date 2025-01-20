60be746c4f49fb1de62543ce6bda3664119a5a0d
Mahdi Shamlou <shamlouprogrammer@gmail.com>
2025-01-20 09:20:36 +0000

    create file config for server config

--- Web_App/main.py
+++ Web_App/main.py
@@ -10,4 +10,19 @@ app.include_router(tasks.router_tasks) # these routes work on details of tasks
 # Check if the script is run directly and start the server
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8585, log_level="info")
+    import configparser
+    import os
+    config_path = os.path.join(os.path.dirname(__file__), 'config', 'server.ini')
+    # Read configuration from server.ini
+    config = configparser.ConfigParser()
+    config.read(config_path)
+
+    print("Config path:", config_path)
+    print(config.sections())
+
+    # Extract settings
+    host = config['server']['host']
+    port = int(config['server']['port'])
+    log_level = config['server']['log_level']
+
+    uvicorn.run(app, host=host, port=port, log_level=log_level)
