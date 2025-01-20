1defd9a956dff3e713fed6995008142acdacc5f0
Mahdi Shamlou <shamlouprogrammer@gmail.com>
2025-01-20 09:04:54 +0000

    add some comment to main files

--- Web_App/main.py
+++ Web_App/main.py
@@ -2,10 +2,10 @@ from fastapi import FastAPI
 import status, machines, submit, tasks
 
 app = FastAPI()
-app.include_router(status.router_status)
-app.include_router(machines.router_machines)
-app.include_router(submit.router_submit)
-app.include_router(tasks.router_tasks)
+app.include_router(status.router_status) # these routes work on status of kvm machines
+app.include_router(machines.router_machines) # these routes work on turn off or on in kvm machines
+app.include_router(submit.router_submit) # these routes work on submit a task
+app.include_router(tasks.router_tasks) # these routes work on details of tasks
 
 # Check if the script is run directly and start the server
 if __name__ == "__main__":
